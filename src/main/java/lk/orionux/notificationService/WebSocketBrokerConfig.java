a4c3fa492a2becf846ca5b6587eaf2e46a0d695c
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-04-18 11:26:47 +0530

    Updated Contnetn

---
+++ src/main/java/lk/orionux/notificationService/WebSocketBrokerConfig.java
@@ -0,0 +1,11 @@
+package lk.orionux.notificationService;
+
+/**
+ * Created by Vishnuka Yahan
+ *
+ * @author : Vishnuka Yahan
+ * @data : 4/18/2024
+ * @project : notificationService
+ */
+public class WebSocketBrokerConfig {
+}
