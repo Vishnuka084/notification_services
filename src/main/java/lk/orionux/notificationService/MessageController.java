839f7971fa056403f570776fd21796c39b2d24aa
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-04-18 11:55:30 +0530

    controller class contnetn updated

--- src/main/java/lk/orionux/notificationService/MessageController.java
+++ src/main/java/lk/orionux/notificationService/MessageController.java
@@ -1,6 +1,13 @@
 package lk.orionux.notificationService;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
 
 /**
  * Created by Vishnuka Yahan
@@ -10,9 +17,18 @@ import org.springframework.stereotype.Controller;
  * @project : notificationService
  */
 
+
 @Controller
 public class MessageController {
 
+    @Autowired
+    SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/application")
+    @SendTo("/all/messages")
+    public Message send(final Message message) throws Exception {
+        return message;
+    }
 
 
 }
