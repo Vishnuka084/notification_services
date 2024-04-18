c39162725fd0b7e86a03e607120d8ef53aa97bee
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-04-18 11:50:12 +0530

    Create Controller class

---
+++ src/main/java/lk/orionux/notificationService/MessageController.java
@@ -0,0 +1,18 @@
+package lk.orionux.notificationService;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Vishnuka Yahan
+ *
+ * @author : Vishnuka Yahan
+ * @data : 4/18/2024
+ * @project : notificationService
+ */
+
+@Controller
+public class MessageController {
+
+
+
+}
